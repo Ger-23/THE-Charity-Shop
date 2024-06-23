fc8763590b851eca7657440054497a6f6bd43280
Gerard Hardiman <hardiman.g@outlook.com>
2024-06-23 13:11:38 +0000

    Added secret key and made migrations

--- the_charity_shop/settings.py
+++ the_charity_shop/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'q8sa0$wu1$^6n&-n!f@cfc!-sx3s3_o+r4zs+k564eo&v(++w='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['thecharityshop.heroku.app', 'localhost',  'thecharityshop-53c973d2e354.herokuapp.com']
 
